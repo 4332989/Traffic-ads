e1e760c37580358b8b9425aeebbfe63f075c1376
4332989 <49079751+4332989@users.noreply.github.com>
2020-08-17 21:03:32 +0800

    Update collect.php

--- collect.php
+++ collect.php
@@ -5,6 +5,7 @@ $events = json_decode($data, true);
 foreach ($events as $event) {
   // Here, you now have each event and can process them how you like
 
+  $laneevent = $_rule_name['rule_name'];
 
 
   process_event($event);
