70e13f33dd0fed628069a90f153589fa86cdf68e
4332989 <49079751+4332989@users.noreply.github.com>
2020-08-17 20:42:29 +0800

    Add files via upload

---
+++ collect.php
@@ -0,0 +1,11 @@
+<?php
+$data = file_get_contents("php://input");
+$events = json_decode($data, true);
+
+foreach ($events as $event) {
+  // Here, you now have each event and can process them how you like
+
+
+
+  process_event($event);
+}
